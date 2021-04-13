82bc700c3d82c6bbb761cf92fb34caf7ac0b899b
mahadi-100 <mahadihasan10030@gmail.com>
2021-04-13 10:22:10 +0600

    add some comments

--- Student-Report-Card-master/src/sortAndSearch/SortByName.java
+++ Student-Report-Card-master/src/sortAndSearch/SortByName.java
@@ -17,22 +17,22 @@ public class SortByName {
     }
 
     private int partition(ArrayList<RecordInfo> list, int low, int high){
-        RecordInfo pivot = list.get(high);
-        int i = (low - 1);
+        RecordInfo pivot = list.get(high);       //set rightmost index as pivot
+        int i = (low - 1);                       //left most index - 1
 
-        for (int j = low; j < high; j++){
-            if (list.get(j).getName().compareTo(pivot.getName()) <= 0){
-                i++;
+        for (int j = low; j < high; j++){         //from leftmost index to rightmost index
+            if (list.get(j).getName().compareTo(pivot.getName()) <= 0){    //index at j less than or equal pivot then
+                i++;                                                       // increment I by one
 
-                RecordInfo temp = list.get(i);
+                RecordInfo temp = list.get(i);                          //i index element swap with j index element
                 list.set(i, list.get(j));
                 list.set(j, temp);
             }
         }
 
-        RecordInfo temp = list.get(i + 1);
+        RecordInfo temp = list.get(i + 1);         //swap pivot element with (i+1) element
         list.set(i+1, list.get(high));
         list.set(high, temp);
-        return (i + 1);
+        return (i + 1);               //return (i+1) index.
     }
 }
